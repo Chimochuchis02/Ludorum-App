f31f5815b31f9d1be0982bc372542dec400ff822
Chimochuchis02 <143348807+Chimochuchis02@users.noreply.github.com>
2026-08-20 08:58:47 -0600

    Feat:improvements for the footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,71 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
 import './App.css'
 
-
+function Carrousel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <ExampleCarouselImage text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
 
 function NavBar() {
   return (
-      <>
+    <>
       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" fixed="top" bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="#home">LUDORUM && CODE</Navbar.Brand>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                  <Nav className="me-auto">
-                    <Nav.Link href="#features">Nosotros</Nav.Link>
-                      <Nav.Link href="#pricing">Proyectos y Clientes</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                              Another action
-                              </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                              <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                  Separated link
-                                  </NavDropdown.Item>
-                                </NavDropdown>
-                  </Nav>
-                    <Nav>
-                      <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                          Tecnologias Utilizadas
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-          </Navbar>
+        <Container>
+          <Navbar.Brand href="#home">LUDORUM && CODE</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Nosotros</Nav.Link>
+              <Nav.Link href="#pricing">Proyectos y Clientes</Nav.Link>
+              <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Tecnologias Utilizadas
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
@@ -47,42 +76,44 @@ function NavBar() {
 function Footer() {
   return (
     <>
-      <footer bg="dark">
-        <Container fluid>
-          <Row className="text-white">
-            
-            <Col>
+      <footer className="page-footer font-small white pt-4" fixed="bottom">
+        <div className="container-fluid text-center text-md-left">
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <h5 className="text-uppercase">Ludorum && Code</h5>
+              <p >Haciendo planes a tu lado, para que nada sea un sueño...
+                sino, una realidad el dia de mañana.</p>
+            </div>
 
-              <h5>LUDORUM && CODE</h5>
+            <hr className="clearfix w-100 d-md-none pb-0" />
 
-            </Col>
+            <div className="col-md-3 mb-md-0 mb-3">
+              <h5 className="text-uppercase">Navegacion</h5>
+              <ul className="list-unstyled">
+                <li><a href="#" id="a">Nosotros</a></li>
+                <li><a href="#" id="a">Proyectos</a></li>
+                <li><a href="#" id="a">Clientes y resultados</a></li>
+                <li><a href="#" id="a">Link 4</a></li>
+              </ul>
+            </div>
 
-            <Col>
+            <div className="col-md-3 mb-md-0 mb-3">
+              <h5 className="text-uppercase">Redes y Contacto</h5>
+              <ul className="list-unstyled">
+                <li><a href="#" id="a">Instagram</a></li>
+                <li><a href="#" id="a">Facebook</a></li>
+                <li><a href="tel:8711324475" id="a">Celular: 8711324475</a></li>
+                <li><a href="#" id="a">contactoludorum@gmail.com</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
-              <h5>Empresa</h5>
+        <div className="footer-copyright text-center py-3">
+          © 2026 All Copyrights Are Reserved: Ludorum && Code.
+        </div>
 
-            </Col>
-
-              <Col>
-
-                  <h5> Informacion </h5>
-
-              </Col>
-
-                <Col>
-                  
-                  <h5>  Redes Sociales </h5>
-
-                </Col>
-
-                <Col>
-
-                  <h5 fixed="bottom"> CopyRight LUDORUM && CODE 2026.|Consulta nuestro Aviso de privacidad </h5>  
-
-                </Col>
-          </Row>
-        </Container>
-      </footer>
+      </footer >
     </>
 
   )
