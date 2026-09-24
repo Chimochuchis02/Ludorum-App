41cd0ad15e06af8267812bcb7ef735bcfb302d99
Chimochuchis02 <143348807+Chimochuchis02@users.noreply.github.com>
2026-09-24 11:04:21 -0600

    feat:Better desing for the footer section

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -8,92 +8,42 @@ library.add(fas, far, fab)
 export default function Footer() {
   return (
     <>
-      <footer
-        className="page-footer font-small white pt-4 sticky-bottom"
-        style={{ backgroundColor: "#350067" }}
-      >
-        <div className="container-fluid text-center text-md-left">
-          <div className="row">
-            <div className="col-md-6 mt-md-0 mt-3 py-5">
-              <h5 className="text-uppercase" id="title_footer">
-                Ludorum && Code
-              </h5>
-              <p id="text_footer">
-                Haciendo planes a tu lado, para que nada sea un sueño...
-                <p id="text_footer">sino, una realidad el dia de mañana.</p>
-              </p>
+      <footer className="page-footer font-small white pt-4 sticky-bottom" style={{ backgroundColor: "#350067", color: "#ffffff" }}>
+        <div class="container">
+          <div class="row align-items-center text-center text-md-start">
+            <div class="col-md-4 mb-4 mb-md-0">
+              <p class="h5 mb-3"> Ludorum && Code</p>
+              <p class="h5"> Te ayudamos a resolver problemas, mediante soluciones tecnologicas, para llevar tu negocio al siguiente nivel</p>
             </div>
 
-            <hr className="clearfix w-100 d-md-none pb-0" />
 
-            <div className="col-md-3 mb-md-0 mb-3 py-2">
-              <h5 className="text-uppercase" class="word_title">
-                Navegacion
-              </h5>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#Us" class="word">
-                    Nosotros
-                  </a>
-                </li>
-                <li>
-                  <a href="#clients" class="word">
-                    Clientes
-                  </a>
-                </li>
-                <li>
-                  <a href="#pricing" class="word">
-                    Planes y precios
-                  </a>
-                </li>
-                <li>
-                  <a href="#form" class="word">
-                    Contacto
-                  </a>
-                </li>
-              </ul>
+            <div class="col-md-4 text-center mb-4 mb-md-0">
+              <img src="assets/32_32_Logo_Ludorum.png" class="rounded-circle shadow mb-2"
+                style={{ width: "120px" }} />
+              <h4 class="fw-bold">Ludorum && Code</h4>
             </div>
 
-            <div className="col-md-3 mb-md-0 mb-3 py-2">
-              <h5 className="text-uppercase" class="word_title">
-                Redes y Contacto
-              </h5>
-              <div className="d-flex gap-3 justify-content-center mt-3">
-                <a
-                  href="https://wa.me/528711324475"
-                  class="social-icon-footer wa-hover text-white bg-white bg-opacity-25 rounded-circle d-flex align-items-center justify-content-center shadow-sm"
-                  style={{ width: "45px", height: "45px", color: "#25D366" }}
-                >
-                  <FontAwesomeIcon icon="fa-brands fa-whatsapp" />
-                </a>
-                <a
-                  href="mailto:angeldominguezlopez030206@gmail.com"
+            <div class="col-md-4 text-center text-md-end">
+              <h5 class="fw-bold mb-3">¡Contactanos por estos medios!</h5>
+              <div class="d-flex justify-content-center justify-content-md-end gap-3">
+                <a href="mailto:angeldl06@gmail.com"
                   class="social-icon-footer envelope-hover text-white bg-white bg-opacity-25 rounded-circle d-flex align-items-center justify-content-center shadow-sm"
-                  style={{ width: "45px", height: "45px", color: "#25D366" }}
-                >
-                  {" "}
-                  <FontAwesomeIcon icon="fa-regular fa-envelope" />{" "}
-                </a>
-                <a
-                  href="tel:52871324475"
+                  style={{ width: "45px", height: "45px", color: "#1877F2" }}><FontAwesomeIcon icon="fa-solid fa-envelope" /></a>
+                <a href="tel:8711324475"
                   class="social-icon-footer phone-hover text-white bg-white bg-opacity-25 rounded-circle d-flex align-items-center justify-content-center shadow-sm"
-                  style={{ width: "45px", height: "45px", color: "#0abbe700" }}
-                >
-                  {" "}
-                  <FontAwesomeIcon icon="fa-solid fa-phone" />{" "}
-                </a>
+                  style={{ width: "45px", height: "45px", color: "#FF1493" }}><FontAwesomeIcon icon="fa-solid fa-phone" /></a>
+                <a href="https://wa.me/8711324475"
+                  class="social-icon-footer wa-hover text-white bg-white bg-opacity-25 rounded-circle d-flex align-items-center justify-content-center shadow-sm"
+                  style={{ width: "45px", height: "45px", color: "#25D366" }}><FontAwesomeIcon icon="fa-brands fa-whatsapp" /></a>
               </div>
             </div>
           </div>
-        </div>
 
-        <div
-          className="d-flex gap-3 justify-content-center"
-          class="word_copyright"
-        >
-          <ul>© 2026 All Copyrights Are Reserved: Ludorum && Code.</ul>
-        </div>
-      </footer>
+          <div class="text-center mt-5 pt-4 border-top border-white border-opacity-25">
+            <small>&copy; Copyright 2026 - Ludorum && Code - Todos los derechos reservados</small>
+          </div>
+        </div >
+      </footer >
     </>
   );
 }
